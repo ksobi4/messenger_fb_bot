617dbc245cb45736c0decaef75957993d4f81959
KSOBI <filip456781337@gmail.com>
2024-12-25 21:45:25 +0100

    second commit

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
